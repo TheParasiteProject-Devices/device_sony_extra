811586c074aea3c5bf55e0d116acf8fd621acb68
someone5678 <someone5678@users.noreply.github.com>
2024-06-04 23:47:37 +0900

    extra: props: Import Xperia specific props for features
    Change-Id: Ic3e6010ef14201d88a5c7ab27bef844ffa6e3536

--- properties.mk
+++ properties.mk
@@ -1,3 +1,9 @@
+# Added system property for RID009076
+PRODUCT_SYSTEM_PROPERTIES += \
+persist.vendor.automaster.mode=true
+persist.vendor.colorgamut.current.mode=1
+persist.vendor.reduce_afterimage.mode=false
+
 # AOSP
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
 ro.audio.spatializer_enabled=true \
@@ -7,8 +13,21 @@ ro.audio.monitorRotation=true
 PRODUCT_VENDOR_PROPERTIES += \
 vendor.360ra.effect=1
 
+# Display(RID012364)
+PRODUCT_SYSTEM_PROPERTIES += \
+persist.vendor.display.dynamic_tone_mapping.mode=true
+
 # Dolby
 PRODUCT_VENDOR_PROPERTIES += \
 ro.vendor.dolby.dax.version=DAX3_3.7.0.8_r1 \
 vendor.audio.dolby.ds2.hardbypass=false \
 vendor.audio.dolby.ds2.enabled=true
+
+# Enable Color-Gamut
+PRODUCT_SYSTEM_PROPERTIES += \
+ro.sys.colorgamut.supported=true
+persist.vendor.colorgamut.mode=1
+
+# X1 default setting
+PRODUCT_SYSTEM_PROPERTIES += \
+persist.service.xrfm.mode=1
