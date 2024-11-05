3e7ce92fe782ee8fffbf4e26c2eff2c594ebe6d2
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-11-05 19:14:28 +0900

    extra: Import starletMagic from vendor/google/gms
    * For overlays extraction
    
    Change-Id: I286ecc4c42f7f655ba186458ea4135c280f142ae

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,10 @@ function blob_fixup() {
             xmlstarlet ed -L --ps -d '//exceptions//exception[@package="com.facebook.appmanager"]' $2
             sed -i '/For docomo/d' $2
             ;;
+        product/overlay/*apk)
+            [ "$2" = "" ] && return 0
+            starletMagic $1 $2 &
+            ;;
         *)
             return 1
             ;;
@@ -84,8 +88,41 @@ function prepare_firmware() {
     :
 }
 
+function starletMagic() {
+    folder=${2/.apk/}
+
+    echo "    "${folder##*/} "\\" >> "${ANDROID_ROOT}/vendor/${VENDOR}/${DEVICE}/overlays.mk"
+
+    apktool -q d "$2" -o $folder -f
+    rm -rf $2 $folder/{apktool.yml,original,res/values/public.xml,unknown}
+
+    cp ${MY_DIR}/overlay-template.txt $folder/Android.bp
+    sed -i "s|dummy|${folder##*/}|g" $folder/Android.bp
+
+    find $folder -type f -name AndroidManifest.xml \
+        -exec sed -i "s|extractNativeLibs\=\"false\"|extractNativeLibs\=\"true\"|g" {} \;
+
+    for file in $(find $folder/res -name *xml \
+            ! -path "$folder/res/raw" \
+            ! -path "$folder/res/drawable*" \
+            ! -path "$folder/res/xml"); do
+        for tag in $(cat exclude-tag.txt); do
+            type=$(echo $tag | cut -d: -f1)
+            node=$(echo $tag | cut -d: -f2)
+            xmlstarlet ed -L -d "/resources/$type[@name="\'$node\'"]" $file
+            xmlstarlet fo -s 4 $file > $file.bak
+            mv $file.bak $file
+        done
+        sed -i "s|\?android:\^attr-private|\@\*android\:attr|g" $file
+        sed -i "s|\@android\:color|\@\*android\:color|g" $file
+        sed -i "s|\^attr-private|attr|g" $file
+    done
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+echo "PRODUCT_PACKAGES += \\" > "${ANDROID_ROOT}/vendor/${VENDOR}/${DEVICE}/overlays.mk"
 
 if [ -z "${ONLY_FIRMWARE}" ]; then
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
